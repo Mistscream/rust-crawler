44d539d61011d4db6c93bacea4a6b680b5a2130b
Marc Schmeling <marcsch@posteo.de>
2018-11-01 13:19:23 +0100

    comments and refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -6,19 +6,8 @@ use self::chrono::prelude::*;
 use self::select::document::Document;
 use self::select::predicate::Name;
 
-//pub fn fat_request<'a>(url: &'a str) -> FatResponse<'a>   {
-//    let res = get(url).unwrap();
-//    let doc = Document::from_read(res).unwrap();
-//    let links: Vec<&str> = doc.find(Name("a")).filter_map(|n| n.attr("href")).collect();
-//
-//    FatResponse {
-//        url: url,
-//        response: res,
-//        document: doc,
-//        links: links,
-//    }
-//}
 
+/// Holds the information of a Response which is important for our crawler
 #[derive(Debug)]
 pub struct ResponseData {
     pub url: String, // make url &str ??
@@ -26,21 +15,19 @@ pub struct ResponseData {
     pub body: Document,
 }
 
+/// Starts a get request to given url and returns ResponseData
 pub fn request(url: &str) -> Option<ResponseData> {
     let response = reqwest::get(url);
-
     if response.is_err() {
         return None;
     }
 
     let mut response = response.unwrap();
-
     if !response.status().is_success() {
         return None;
     }
 
     let body = response.text();
-
     if body.is_err() {
         return None;
     }
@@ -54,6 +41,7 @@ pub fn request(url: &str) -> Option<ResponseData> {
     })
 }
 
+/// Finds all links within a Document
 pub fn find_links<'a>(body: &'a Document) -> Vec<&'a str> {
     body.find(Name("a"))
         .filter_map(|a| a.attr("href"))
