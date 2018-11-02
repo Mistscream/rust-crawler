037e7e13d0277846abe9ae0e9f2478ac20fa9219
Marc Schmeling <marcsch@posteo.de>
2018-11-02 22:11:40 +0100

    refactoring

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,36 +1,30 @@
-mod request;
 mod link;
 mod report;
-
+mod request;
 
 /// Starts the crawling
-pub fn run(start_urls: Vec<String>) {
+pub fn run(start_urls: &mut Vec<String>) {
     let mut url_queue = link::UrlQueue::new();
     let mut responses: Vec<request::ResponseData> = vec![];
 
     url_queue.add(start_urls);
 
     loop {
-        println!("{} urls in queue: starting requests...", url_queue.len());
+        println!("Got {} urls in queue: starting requests...", url_queue.len());
         for url in url_queue.get().iter() {
             println!("Sending request to {}", url);
             let response = request::get(url);
-            match response {
-                Some(r) => {
-                    responses.push(r);
-                    println!("Request successful");
-                }
-                None => println!("Request failed")
+            if response.is_some() {
+                responses.push(response.unwrap());
+                println!("Request successful");
             }
         }
 
-        println!(
-            "{} responses from requests: starting link search...",
-            responses.len()
-        );
+        println!("Got {} responses: starting link search...", responses.len());
         for response in responses.iter() {
-            let links = link::process(response.get_body());
-            url_queue.add(links);
+            let mut links = link::process(response.get_body());
+            println!("Found {} new links", links.len());
+            url_queue.add(&mut links);
         }
     }
 }
