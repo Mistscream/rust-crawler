c4193dfd9d3a14f549592697934b1d98b56ed2d2
Marc Schmeling <marcsch@posteo.de>
2018-11-09 10:44:20 +0100

    refactored to iterator stead of for loop

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -15,6 +15,7 @@ pub fn run(start_url: &str) {
 
     loop {
         // make requests to all urls in queue and store responses
+        // let mut new_responses = send_request_conc(&mut url_queue);
         let mut new_responses = send_request(&mut url_queue);
         responses.append(&mut new_responses);
 
@@ -38,15 +39,16 @@ pub fn run(start_url: &str) {
 fn send_request(url_q: &mut Vec<Url>) -> Vec<Response> {
     println!("{} urls in queue, sending requests", url_q.len());
 
-    let mut responses: Vec<Response> = vec![];
+    let responses: Vec<Response> = url_q
+        .iter()
+        .filter(|u| !u.is_visited())
+        .map(|u| http::get(u.get_string()))
+        .filter(|r| r.is_some())
+        .map(|r| r.unwrap())
+        .collect();
+
     for url in url_q.iter_mut() {
-        if !url.is_visited() {
-            let response = http::get(url.get_string());
-            if response.is_some() {
-                responses.push(response.unwrap());
-                url.set_visited(true);
-            }
-        }
+        url.set_visited(true);
     }
 
     println!("{} new responses", responses.len());
