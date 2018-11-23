265df2d057351135708ed3b553c6d2d1da377759
Marc Schmeling <s0553407@htw-berlin.de>
2018-11-23 10:13:40 +0100

    removed id

--- src/lib/report/mod.rs
+++ src/lib/report/mod.rs
@@ -1,11 +1,10 @@
-use chrono::{Date, DateTime, Utc};
+use chrono::{prelude::*, DateTime, Utc};
 use lib::http::Response;
 
 pub struct Report {
     url: String,
-    requested_at: DateTime<Utc>,
-    date: DateTime<Utc>,
-    id: u32,
+    requested_at: String,
+    date: String,
     title: String,
     location: String,
     text: String,
@@ -13,15 +12,83 @@ pub struct Report {
 
 impl Report {
     pub fn new(response: &Response) -> Report {
+        let body = scraper::Html::parse_fragment(response.get_body());
         Report {
             url: String::from(response.get_url()),
-            requested_at: *response.get_time(),
-            date: *response.get_time(),
-            id: 0,
-            title: get_title(response.get_body()),
-            location: get_location(response.get_body()),
+            requested_at: response.get_time().to_rfc3339(),
+            date: Report::parse_date(&body),
+            title: Report::parse_title(&body),
+            location: Report::parse_location(&body),
             text: String::from("text"),
         }
+    }
+
+    fn parse_date(body: &scraper::Html) -> String {
+        let div = scraper::Selector::parse("div").unwrap();
+        let date = body
+            .select(&div)
+            .filter(|h| h.value().attr("class").is_some())
+            .filter(|h| h.value().attr("class").unwrap() == "polizeimeldung")
+            .map(|elem| elem.inner_html())
+            .nth(0);
+
+        let date = match date {
+            Some(d) => {
+                let day = &d[19..21].parse::<u32>();
+                let day = match day {
+                    Ok(d) => *d,
+                    Err(_) => 0,
+                };
+                let month = &d[22..24].parse::<u32>();
+                let month = match month {
+                    Ok(m) => *m,
+                    Err(_) => 0,
+                };
+                let year = &d[25..29].parse::<i32>();
+                let year = match year {
+                    Ok(y) => *y,
+                    Err(_) => 0,
+                };
+                Utc.ymd(year, month, day).and_hms(0, 0, 0)
+            }
+            None => Utc.ymd(0, 0, 0).and_hms(0, 0, 0),
+        };
+
+        date.to_rfc3339()
+    }
+
+    fn parse_title(body: &scraper::Html) -> String {
+        let h1 = scraper::Selector::parse("h1").unwrap();
+        let title = body
+            .select(&h1)
+            .filter(|h| h.value().attr("class").is_some())
+            .filter(|h| h.value().attr("class").unwrap() == "title")
+            .map(|elem| elem.inner_html())
+            .nth(0);
+
+        let title = match title {
+            Some(t) => t,
+            None => String::from("no title"),
+        };
+
+        title
+    }
+
+    fn parse_location(body: &scraper::Html) -> String {
+        let div = scraper::Selector::parse("div").unwrap();
+        let location = body
+            .select(&div)
+            .filter(|h| h.value().attr("class").is_some())
+            .filter(|h| h.value().attr("class").unwrap() == "polizeimeldung")
+            .map(|elem| elem.inner_html())
+            .nth(1);
+
+        let location = match location {
+            Some(l) => l,
+            None => String::from("no location"),
+        };
+
+        location
     }
 }
 
