427ef697f15d5aea3c3cdc058d62bb953a2e36b1
Marc Schmeling <s0553407@htw-berlin.de>
2018-11-25 18:23:52 +0100

    made url_queue a hashmap and removed struct Url

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,36 +1,47 @@
-mod request;
 mod report;
+mod request;
 mod url;
 
-use self::report::Report;
-use self::url::Url;
+use chrono::Utc;
 use rayon::prelude::*;
+use std::collections::HashMap;
 
-pub fn run(start_urls: Vec<String>) {
-    let mut url_queue: Vec<Url> = vec![];
-    let mut reports: Vec<Report> = vec![];
+pub fn run(urls: Vec<String>) {
+    let mut url_queue = HashMap::new();
+    for url in urls {
+        url_queue.insert(String::from(url), false);
+    }
 
-    url_queue.append(&mut start_urls.iter().map(|s| Url::new(s)).collect());
-    let start_time = chrono::Utc::now();
+    let mut reports = Vec::new();
+    let start_time = Utc::now();
 
     loop {
         // make requests to all urls in queue
         let bodies: Vec<String> = url_queue
-            .iter()
-            .filter(|u| !u.is_visited())
+            .keys()
             .filter_map(|u| request::get(u.as_str()))
             .collect();
 
         // mark all urls in queue as visited
-        url_queue.iter_mut().for_each(|u| u.set_visited(true));
+        url_queue.iter_mut().for_each(|e| *e.1 = false);
 
         // extract urls from bodies and append to url queue
-        url_queue.append(&mut bodies.par_iter().flat_map(|u| url::from_html(u)).collect());
-        url_queue.sort_unstable();
-        url_queue.dedup();
+        bodies
+            .par_iter()
+            .flat_map(|b| url::from_html(&b))
+            .filter(|u| !url_queue.contains_key(u))
+            .collect::<Vec<String>>()
+            .iter()
+            .map(|u| url_queue.insert(String::from(u.as_str()), false))
+            .for_each(|_| ());
 
         // extract new reports from bodies and append to reports
-        reports.append(&mut bodies.par_iter().flat_map(|b| report::from_html(b)).collect());
+        reports.append(
+            &mut bodies
+                .par_iter()
+                .flat_map(|b| report::from_html(b))
+                .collect(),
+        );
 
         // stop crawling when there are no unvisited urls
         if url_queue.len() > 1 {
--- src/lib/url.rs
+++ src/lib/url.rs
@@ -1,50 +1,4 @@
-pub struct Url {
-    url: String,
-    visited: bool,
-}
-
-impl Url {
-    pub fn new(url: &str) -> Url {
-        Url {
-            url: String::from(url),
-            visited: false,
-        }
-    }
-
-    pub fn is_visited(&self) -> bool {
-        self.visited
-    }
-
-    pub fn set_visited(&mut self, status: bool) {
-        self.visited = status;
-    }
-
-    pub fn as_str(&self) -> &str {
-        &self.url
-    }
-}
-
-impl Ord for Url {
-    fn cmp(&self, other: &Url) -> std::cmp::Ordering {
-        self.url.cmp(&other.url)
-    }
-}
-
-impl PartialEq for Url {
-    fn eq(&self, other: &Url) -> bool {
-        self.url == other.url
-    }
-}
-
-impl PartialOrd for Url {
-    fn partial_cmp(&self, other: &Url) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Eq for Url {}
-
-pub fn from_html(body: &str) -> Vec<Url> {
+pub fn from_html(body: &str) -> Vec<String> {
     let base_url = "https://www.berlin.de";
     let body = scraper::Html::parse_fragment(body);
     let anker_tags = scraper::Selector::parse("a").unwrap();
@@ -59,6 +13,5 @@ pub fn from_html(body: &str) -> Vec<Url> {
                 s
             }
         }).filter(|s| s.starts_with("https://www.berlin.de/polizei/polizeimeldungen/archiv/20"))
-        .map(|s| Url::new(&s))
-        .collect::<Vec<Url>>()
+        .collect::<Vec<String>>()
 }
