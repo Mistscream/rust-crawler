71c3bec26eec7d4c5ccd89fe77afed278573a815
Marc Schmeling <s0553407@htw-berlin.de>
2018-11-25 20:09:33 +0100

    changed start urls

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,11 @@ extern crate scraper;
 mod lib;
 
 fn main() {
-    let u1 = String::from("https://www.berlin.de/polizei/polizeimeldungen/archiv/2014");
-    let u2 = String::from("https://www.berlin.de/polizei/polizeimeldungen/archiv/2015");
-    let u3 = String::from("https://www.berlin.de/polizei/polizeimeldungen/archiv/2016");
-    let u4 = String::from("https://www.berlin.de/polizei/polizeimeldungen/archiv/2017");
-    let u5 = String::from("https://www.berlin.de/polizei/polizeimeldungen/archiv/2018");
+    let u1 = String::from("https://www.berlin.de/polizei/polizeimeldungen/archiv/2014/?page_at_1_0=1");
+    let u2 = String::from("https://www.berlin.de/polizei/polizeimeldungen/archiv/2015/?page_at_1_0=1");
+    let u3 = String::from("https://www.berlin.de/polizei/polizeimeldungen/archiv/2016/?page_at_1_0=1");
+    let u4 = String::from("https://www.berlin.de/polizei/polizeimeldungen/archiv/2017/?page_at_1_0=1");
+    let u5 = String::from("https://www.berlin.de/polizei/polizeimeldungen/archiv/2018/?page_at_1_0=1");
     let urls = vec![u1, u2, u3, u4, u5];
     // let urls = vec![u1];
     // nur archiv seiten durchsuchen!!!
